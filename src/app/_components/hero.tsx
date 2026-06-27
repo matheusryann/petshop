0bdc8a6505a54576aab9d9e4e76a1f946b74c2a8
matheusryann <ryanncarreiro09@gmail.com>
2026-06-27 11:40:34 -0300

    feat: add Hero component for pet care services section with responsive images and WhatsApp contact link

---
+++ src/app/_components/hero.tsx
@@ -0,0 +1,78 @@
+import { WhatsappLogo } from '@phosphor-icons/react/dist/ssr'
+import Image from 'next/image';
+import dogImg from '../../public/images/dog-hero.png'
+import catHero from '../../public/images/cat-hero.png'
+
+export function Hero() {
+  return (
+    <section className="bg-[#E84C3D] text-white relative overflow-hidden">
+
+      <div>
+        <Image
+          src={dogImg}
+          alt="Foto do Cachorro"
+          fill
+          sizes="100vw"
+          className="object-cover opacity-60 lg:hidden"
+          quality={100}
+          priority
+        />
+        <div className="absolute inset-0 bg-black bg-opacity-50 md:hidden"></div>
+      </div>
+
+      <div className="container mx-auto pt-16 pb-16 md:pb-0 px-4 relative">
+
+      <article className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+
+        <div className="space-y-6 ">
+
+          <h1 className="text-3xl md:text-4xl font-bold leading-10">Seu pet merece cuidado, carinho e atenção especial</h1>
+          <p className="lg:text-lg">
+            Oferecemos os melhores serviços para garantir o bem-estar e a
+            felicidade do seu amigo de quatro patas.
+          </p>
+            <a href="#"
+            className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit"
+            >
+                <WhatsappLogo className="w-5 h-5 gap-2" />
+                Contato via WhatsApp
+            </a>
+
+          <div>
+            <p className="text-sm mb-4">
+                <b className="bg-black text-white px-2 py-1 rounded-md">5%</b> de desconto na primeira compra.
+            </p>
+
+            <div className="flex mt-4">
+              <div className="w-32 hidden lg:block">
+              <Image
+                src={catHero}
+                alt="Foto do Gato"
+                fill
+                quality={100}
+              />
+               </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="hidden md:block h-full relative">
+          <Image
+            src={dogImg}
+            alt="Foto do Cachorro"
+            fill
+            sizes="(max-width: 768px) 0vw, 50vw"
+            className="object-contain"
+            quality={100}
+            priority
+          />
+        </div>
+
+      </article>
+      </div>
+
+
+    </section>
+  );
+}
