2edf151ba45d1d31b0d1d4dc0ae3c9945ddadc8f
matheusryann <ryanncarreiro09@gmail.com>
2026-06-27 17:42:41 -0300

    feat: enhance About component with contact options and store address

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import about1 from "../../../public/about-1.png";
 import about2 from "../../../public/about-2.png";
-import { Check } from "lucide-react";
+import { Check, MapPin } from "lucide-react";
+import {  WhatsappLogo } from '@phosphor-icons/react/dist/ssr'
 
 export function About() {
   return (
     <section className="bg-[#FDF6Ec] py-16">
-      <div className="container mx-auto grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
         <div className="relative">
-          <div className="relative w-full h-[400px] lg:w-[500px] h-[500px] rounded-lg border-white overflow-hidden">
+          <div className="relative w-full h-[400px] rounded-lg border-white overflow-hidden">
             <Image
               src={about1}
               alt="Foto de um cachorro"
@@ -51,6 +53,23 @@ export function About() {
               Qualidade é nossa prioridade. Mais de 1000 clientes satisfeitos
             </li>
           </ul>
+          <div className="flex gap-2">
+            <a 
+          className="bg-[#E24E42] rounded-md text-white flex items-center justify-center w-fit gap-2 px-2 py-2 hover:bg-red-700 hover:scale-[1.04] transition-all duration-500"
+          href="#"
+          >
+            <WhatsappLogo className="text-white" />
+            Contato via WhatsApp
+          </a>
+          <a 
+          className="rounded-md flex items-center justify-center w-fit gap-2 px-2 py-2 hover:text-yellow-900 hover:scale-[1.03] transition-all duration-200"
+          href="#"
+          >
+            <MapPin className="text-black w-5 h-5" />
+            Endereço da Loja
+          </a>
+          </div>
+        </div>
         </div>
       </div>
     </section>
